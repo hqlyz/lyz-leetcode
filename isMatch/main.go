b4d905e4a5db0474da5f792230dde445e59f7971
lyz <lyz@gmail.com>
2021-03-25 11:12:45 +0800

    update

--- isMatch/main.go
+++ isMatch/main.go
@@ -3,53 +3,50 @@ package main
 import "fmt"
 
 func isMatch(s string, p string) bool {
-	if s == "" && p == "" {
-		return true
-	} else if s == "" || p == "" {
-		return false
+	m, n := len(s), len(p)
+	// s 第 i 个字符和 p 第 j 个字符是否匹配
+	dp := make([][]bool, m+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]bool, n+1)
 	}
 
-	sp, pp := 0, 0
-	var pre byte = 0
-	for sp < len(s) && pp < len(p) {
-		switch p[pp] {
-		case '.':
-			sp++
-			pp++
-		case '*':
-			if pre == 0 {
-				return true
+	dp[0][0] = true
+	for i := 0; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if p[j-1] == '*' {
+				dp[i][j] = dp[i][j] || dp[i][j-2]
+				if match(i, j-1, s, p) {
+					dp[i][j] = dp[i][j] || dp[i-1][j]
+				}
+			} else {
+				if match(i, j, s, p) {
+					dp[i][j] = dp[i][j] || dp[i-1][j-1]
+				}
 			}
-			for sp < len(s) && s[sp] == pre {
-				sp++
-			}
-			pp++
-		default:
-			pre = p[pp]
-			if pp + 1 < len(p) && p[pp+1] == '*' {
-				pp++
-				continue
-			}
-
-			if s[sp] != p[pp] {
-				return false
-			}
-			sp++
-			pp++
 		}
 	}
 
-	if sp == len(s) && pp == len(p) {
+	return dp[m][n]
+}
+
+func match(i, j int, s, p string) bool {
+	if i == 0 {
+		return false
+	}
+
+	if p[j-1] == '.' {
 		return true
 	}
-	return false
+
+	return s[i-1] == p[j-1]
 }
 
 func main() {
-	// fmt.Println(isMatch("", ""))	// true
-	// fmt.Println(isMatch("", "1"))	// false
-	// fmt.Println(isMatch("1", ""))	// false
-	// fmt.Println(isMatch("aab", "c*a*b"))	// true
-	// fmt.Println(isMatch("mississippi", "mis*is*p*."))	// false
-	fmt.Println(isMatch("ab", ".*c"))
+	fmt.Println(isMatch("", ""))                      // true
+	fmt.Println(isMatch("", "1"))                     // false
+	fmt.Println(isMatch("1", ""))                     // false
+	fmt.Println(isMatch("aab", "c*a*b"))              // true
+	fmt.Println(isMatch("mississippi", "mis*is*p*.")) // false
+	fmt.Println(isMatch("abc", ".*c"))                // true
+	fmt.Println(isMatch("ab", ".*c"))                 // false
 }
