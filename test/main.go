2b424c679bc62b195dd0b341a62a59b8ae823984
lyz <lyz@gmail.com>
2021-05-10 17:29:41 +0800

    add test

---
+++ test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// f, err := os.Create("trace.out")
+	// if err != nil {
+	// 	return
+	// }
+	// defer f.Close()
+
+	// err = trace.Start(f)
+	// if err != nil {
+	// 	return
+	// }
+	// defer trace.Stop()
+
+	fmt.Println("Hello world")
+}
