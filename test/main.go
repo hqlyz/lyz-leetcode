df96d0664a231e7c66b3c99917b96c340f7890eb
lyz <lyz@gmail.com>
2021-05-12 17:12:25 +0800

    update

--- test/main.go
+++ test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
@@ -22,13 +23,12 @@ type Once struct {
 }
 
 func (o *Once) Do(f func()) {
-	if o.done == 1 {
-		return
-	}
-	o.m.Lock()
-	defer o.m.Unlock()
-	if o.done == 0 {
-		o.done = 1
-		f()
+	if atomic.LoadUint32(&o.done) == 0 {
+		o.m.Lock()
+		defer o.m.Unlock()
+		if o.done == 0 {
+			defer atomic.StoreUint32(&o.done, 1)
+			f()
+		}
 	}
 }
